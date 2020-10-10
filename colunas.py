cc9b9e26e80ef7cfe3f9973de30efeec98bf45b2
gabriel3421 <gabrielsouza.n.s@hotmail.com>
2020-10-10 19:43:52 -0300

    finalizando classification

--- colunas.py
+++ colunas.py
@@ -1,4 +1,4 @@
-a = 3
+# -*- coding: utf-8 -*-
 tracado_via = {
   'Reta': 0,
   'Curva': 1,
@@ -16,3 +16,92 @@ uso_solo = {
   'Rural': 0,
   'Urbano': 1,
 }
+
+causa_acidente = {
+  'Outras':0,
+  'Animais na Pista':1,
+  'Não guardar distância de segurança':2,
+  'Falta de atenção':3,
+  'Velocidade Incompatível':4,
+  'Desobediência à sinalização':5,
+  'Defeito Mecânico no Veículo':6,
+  'Ingestão de Álcool':7,
+  'Dormindo':8,
+  'Ultrapassagem Indevida':9,
+  'Defeito na Via':10,
+  'Falta de Atenção à Condução':11,
+  'Desobediência às normas de trânsito pelo condutor':12,
+  'Condutor Dormindo':13,
+  'Falta de Atenção do Pedestre':14,
+  'Pista Escorregadia':15,
+  'Avarias e/ou desgaste excessivo no pneu':16,
+  'Sinalização da via insuficiente ou inadequada':17,
+  'Mal Súbito':18,
+  'Carga excessiva e/ou mal acondicionada':19,
+  'Restrição de Visibilidade':20,
+  'Objeto estático sobre o leito carroçável':21,
+  'Deficiência ou não Acionamento do Sistema de Iluminação/Sinalização do Veículo':22,
+  'Fenômenos da Natureza':23,
+  'Ingestão de Substâncias Psicoativas':24,
+  'Agressão Externa':25,
+  'Desobediência às normas de trânsito pelo pedestre':26,
+  'Ingestão de álcool e/ou substâncias psicoativas pelo pedestre':27
+}
+
+tipo_pista = {
+  'Simples':0,
+  'Dupla':1,
+  'Múltipla':2
+}
+tipo_acidente = {
+   'Atropelamento de Animal': 18,
+   'Atropelamento de Pedestre': 20,
+   'Atropelamento de animal': 2,
+   'Atropelamento de pessoa': 8,
+   'Capotamento': 10,
+   'Colisão Transversal': 5,
+   'Colisão com bicicleta': 13,
+   'Colisão com objeto em movimento': 22,
+   'Colisão com objeto estático': 17,
+   'Colisão com objeto fixo': 7,
+   'Colisão com objeto móvel': 14,
+   'Colisão frontal': 0,
+   'Colisão lateral': 3,
+   'Colisão transversal': 21,
+   'Colisão traseira': 4,
+   'Danos Eventuais': 9,
+   'Danos eventuais': 24,
+   'Derramamento de Carga': 12,
+   'Derramamento de carga': 25,
+   'Engavetamento': 23,
+   'Incêndio': 15,
+   'Queda de motocicleta / bicicleta / veículo': 11,
+   'Queda de ocupante de veículo': 16,
+   'Saída de Pista': 1,
+   'Saída de leito carroçável': 19,
+   'Tombamento': 6
+}
+condicao_metereologica = {
+  'Ceu Claro':0,
+  'Chuva':1,
+  'Nublado':2,
+  'Sol':3,
+  'Nevoeiro/neblina':4,
+  'Ignorada':5,
+  'Granizo':7,
+  'Vento':6,
+  'Neve':8,
+  'Garoa/Chuvisco':9,
+  'Céu Claro':10,
+  'Nevoeiro/Neblina':11
+}
+fase_dia = {
+  'Pleno dia':0,
+  'Plena noite':1,
+  'Anoitecer':3,
+  'Amanhecer':2,
+}
+sentido_via = {
+  'Crescente':1,
+  'Decrescente':0
+  }
--- i.py
+++ i.py
@@ -1,15 +1,116 @@
-import colunas
+# -*- coding: utf-8 -*-
+from colunas import *
+from joblib import load
 import streamlit as st
 import pandas as pd
 from sklearn import datasets
 from sklearn.ensemble import RandomForestClassifier
-import sys  
 
-reload(sys)  
-sys.setdefaultencoding('utf8')
+clf = load('Mlp_classificador.joblib')
 
-st.write("Flores de iris")
 st.sidebar.header("Input Params")
+causa = st.sidebar.selectbox(
+    'Causa do Acidente',
+    ('Outras',
+     'Animais na Pista',
+      'Não guardar distância de segurança',
+      'Falta de atenção',
+      'Velocidade Incompatível',
+      'Desobediência à sinalização',
+      'Defeito Mecânico no Veículo',
+      'Ingestão de Álcool',
+      'Dormindo',
+      'Ultrapassagem Indevida',
+      'Defeito na Via',
+      'Falta de Atenção à Condução',
+      'Desobediência às normas de trânsito pelo condutor',
+      'Condutor Dormindo',
+      'Falta de Atenção do Pedestre',
+      'Pista Escorregadia',
+      'Avarias e/ou desgaste excessivo no pneu',
+      'Sinalização da via insuficiente ou inadequada',
+      'Mal Súbito',
+      'Carga excessiva e/ou mal acondicionada',
+      'Restrição de Visibilidade',
+      'Objeto estático sobre o leito carroçável',
+      'Deficiência ou não Acionamento do Sistema de Iluminação/Sinalização do Veículo',
+      'Fenômenos da Natureza',
+      'Ingestão de Substâncias Psicoativas',
+      'Agressão Externa',
+      'Desobediência às normas de trânsito pelo pedestre',
+      'Ingestão de álcool e/ou substâncias psicoativas pelo pedestre')
+)
+tipoacidente = st.sidebar.selectbox(
+    'Tipo Acidente',
+    ('Atropelamento de Animal',
+       'Atropelamento de Pedestre',
+       'Atropelamento de animal',
+       'Atropelamento de pessoa',
+       'Capotamento',
+       'Colisão Transversal',
+       'Colisão com bicicleta',
+       'Colisão com objeto em movimento',
+       'Colisão com objeto estático',
+       'Colisão com objeto fixo',
+       'Colisão com objeto móvel',
+       'Colisão frontal',
+       'Colisão lateral',
+       'Colisão transversal',
+       'Colisão traseira',
+       'Danos Eventuais',
+       'Danos eventuais',
+       'Derramamento de Carga',
+       'Derramamento de carga',
+       'Engavetamento',
+       'Incêndio',
+       'Queda de motocicleta / bicicleta / veículo',
+       'Queda de ocupante de veículo',
+       'Saída de Pista',
+       'Saída de leito carroçável',
+       'Tombamento')
+)
+
+metereologica = st.sidebar.selectbox(
+    'Condição Metereologica',
+    ('Ceu Claro',
+      'Chuva',
+      'Nublado',
+      'Sol',
+      'Nevoeiro/neblina',
+      'Ignorada',
+      'Granizo',
+      'Vento',
+      'Neve',
+      'Garoa/Chuvisco',
+      'Céu Claro',
+      'Nevoeiro/Neblina')
+)
+
+
+fasedia = st.sidebar.selectbox(
+    'Fase do dia',
+    ('Pleno dia',
+      'Plena noite',
+      'Anoitecer',
+      'Amanhecer',
+      'Plena Noite',
+      'Valor não definido')
+)
+
+
+sentidovia = st.sidebar.selectbox(
+    'Sentido da Via',
+    ('Crescente',
+    'Decrescente')
+)
+
+tipopista = st.sidebar.selectbox(
+    'Tipo da Pista',
+    ('Simples',
+      'Dupla',
+      'Múltipla')
+)
+
 
 tracado = st.sidebar.selectbox(
     'Tracado_via',
@@ -67,6 +168,7 @@ tipos_veiculos = st.sidebar.multiselect(
         'MotorHome',
         'Reboque',
         'Semi/Reboque',
+        'Trator',
         'Side/car',
         'Trem/bonde',
         'Utilitario',
@@ -78,35 +180,96 @@ tipos_veiculos = st.sidebar.multiselect(
 if (len(tipos_veiculos) > quant_veiculos):
     st.sidebar.warning('Selecione a mesma quantidade dos veiculos envolvidos, caso selecione um numero maior somente os '+ str(quant_veiculos) +' primeiros seram contabilizados.')
 
-sepal_lenght = st.sidebar.slider('Sepal Lenght',4.3,7.9,5.4)
-sepal_width = st.sidebar.slider('sepal width',2.0,4.4,3.4)
-petal_lenght = st.sidebar.slider('petal Lenght',1.0,6.9,1.3)
-petal_width = st.sidebar.slider('petal width',0.1,2.5,0.2)
-data = {
-    'sepal_lenght' : sepal_lenght,
-    'sepal_width' : sepal_width,
-    'petal_lenght' : petal_lenght,
-    'petal_width' : petal_width
+def tp_automoveis(tipos_veiculos):
+    automovel, bicicleta, carrinhodemao, carroça = 0,0,0,0
+    chassi, ciclomotor,microonibus,motocicletas = 0,0,0,0
+    motorHome,reboque,semi_reboque,sidecar = 0,0,0,0
+    trembonde, utilitario, veiculomediogrande,onibus,trator,outros = 0,0,0,0,0,0
+
+    tpv = [automovel, bicicleta, carrinhodemao, carroça,
+            chassi, ciclomotor,microonibus,motocicletas,
+            motorHome,reboque,semi_reboque,sidecar,
+            trembonde, utilitario, veiculomediogrande,onibus,trator,outros]
+
+    tpv2 = ['Automovel',
+            'Bicicleta',
+            'Carrinho de mao',
+            'Carroça/charrete',
+            'Chassi/Plataforma',
+            'Ciclomotor/Triciclo/Quadriciclo/Motoneta',
+            'Microonibus',
+            'Motocicletas',
+            'MotorHome',
+            'Reboque',
+            'Semi/Reboque',
+            'Side/car',
+            'Trem/bonde',
+            'Utilitario',
+            'veiculo de medio/grande',
+            'Onibus',
+            'Trator',
+            'Outros']
+
+
+    for i in range(len(tipos_veiculos)):
+        for j in range(len(tpv2)):
+            if(tipos_veiculos[i] == tpv2[j]):
+                tpv[j] = 1;
+                print(tipos_veiculos[i])
+
+    return tpv
+
+tpv = tp_automoveis(tipos_veiculos)
+datas = {
+    'causa_acidente': causa_acidente[causa],
+    'tipo_acidente': tipo_acidente[tipoacidente],
+    'fase_dia': fase_dia[fasedia],
+    'sentido_via': sentido_via[sentidovia],
+    'condicao_metereologica': condicao_metereologica[metereologica],
+    'tipo_pista': tipo_pista[tipopista],
+    'tracado_via': tracado_via[tracado],
+    'uso_solo': uso_solo[solo],
+    'pessoas': quant_pessoas,
+    'mortos': quant_mortos,
+    'veiculos': quant_veiculos,
+    '0':0,
+    'Automóvel': tpv[0],
+    'Bicicleta':tpv[1],
+    'Carro de mão':tpv[2],
+    'Carro-de-mao':tpv[2],
+    'Carroça-charrete':tpv[3],
+    'Chassi-plataforma':tpv[4],
+    'Ciclomotor-Triciclo-Quadriciclo-Motoneta':tpv[5],
+    'Microônibus':tpv[6],
+    'Motocicletas':tpv[7],
+    'Motor-Casa':tpv[8],
+    'Reboque':tpv[9],
+    'Semi-Reboque':tpv[10],
+    'Side-car':tpv[11],
+    'Trem-bonde':tpv[12],
+    'Utilitário':tpv[13],
+    'veiculo de medio-grande':tpv[14],
+    'Ônibus':tpv[15],
+    'Trator':tpv[16],
+    'Outros':tpv[17]
+    
 }
-features = pd.DataFrame(data,index=[0])
 
-st.subheader('User input params')
-st.write(features)
+features = pd.DataFrame(datas,index=[0])
+print(features)
 
-iris = datasets.load_iris()
-X = iris.data
-Y = iris.target
-clf = RandomForestClassifier()
-clf.fit(X,Y)
+st.header("Classificador")
 
 prediction = clf.predict(features)
 prediction_proba = clf.predict_proba(features)
-
-st.subheader("Class labels and their corresponding index number")
-st.write(iris.target_names)
-
-st.subheader("pedriction")
-st.write(iris.target_names[prediction])
-
-st.subheader("pedriction probability")
-st.write(prediction_proba)
+print(prediction)
+print(prediction_proba)
+st.subheader("Predriction")
+st.write(pd.DataFrame({'Conclusao': prediction}))
+st.subheader("Predriction probability")
+st.write(pd.DataFrame(
+    {
+        'Com Vítimas Fatais': prediction_proba[0][0],
+        'Com Vítimas Feridas': prediction_proba[0][1],
+        'Sem Vítimas': prediction_proba[0][2],
+    }, index=[0]))
